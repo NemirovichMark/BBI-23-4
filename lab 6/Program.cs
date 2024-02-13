00e817d6f40d4b0c9b011a15c8a3e8aeabbbfab5
RagozinArtem <m2308500@edu.misis.ru>
2024-02-13 13:28:01 +0300

    ghhg

--- lab 6/Program.cs
+++ lab 6/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿struct sorevnovania
+{
+    public int firsttry;
+    public int secondtry;
+    public string Surname;
+    public int besttry;
+}
+class Program
+{
+
+    static void Main()
+    {
+        sorevnovania[] all=new sorevnovania[3];
+        string[] familii = new string[] { "Ivanov", "Petrov", "Sidorov" };
+        int[] pervii = new int[] {166, 177 , 168};
+        int[] vtorii = new int[] { 168, 180, 190 };
+        int[] besttry
+
+        
+    }
+}
