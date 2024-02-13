1588af420ad5b53af96ec47cb21fe6223869361a
Yamaletdinova <5m_alsu@mail.ru>
2024-02-13 13:12:55 +0300

    изменение

---
+++ _6th_lab/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6th_lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
