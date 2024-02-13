c15691b55fde80336b8ebfd9a5cd72d7cdabf55a
erler-iana <iana_erl@mail.ru>
2024-02-13 17:14:17 +0700

    commit

---
+++ 6 лабораторная работа/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_лабораторная_работа
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
