98aa45dc82033ff5e8250732cfeed549d5820cd2
PodzemelnyiAlexandr <m2310266@edu.misis.ru>
2024-02-25 20:02:32 +0300

    level 1 done

---
+++ LR-6/Lvl1.cs
@@ -0,0 +1,87 @@
+// Номер 3
+// Радиокомпания провела опрос слушателей по вопросу: «Кого вы считаете
+// человеком года?». Определить пять наиболее часто встречающихся ответов и их
+// долей (в процентах от общего количества ответов).
+
+using System.Diagnostics.Contracts;
+
+string[] names = ["Petya", "Vanya", "Ruslan", "Vova", "Fedor", "Petya", "Petya", "Ruslan", "Fedor", "Sergei", "Max", "Max", "Petya"];
+string[] NamesSorted = new string[names.Length];  // массив с уникальными именами
+int[] NamesCalls = new int[names.Length]; // массив с количеством повторений этих имен
+int count = 0, last = 0;
+for (int i = 0; i < names.Length; i++)  // сортировка элементов в массиве по количеству повторяющихся элементов
+{
+    if (InArray(names[i], NamesSorted) == -1)
+    {
+        NamesSorted[last] = names[i];
+        NamesCalls[last] += 1;
+        last++;
+    }
+    else NamesCalls[InArray(names[i], NamesSorted)] += 1;
+    count++;
+}
+
+int InArray(string name, string[] names)  // метод, позволяющий узнать, есть ли элемент в массиве
+{
+    for (int i = 0; i < count; i++) if (names[i] == name)
+            return i;
+    return -1;
+}
+
+Person[] people = new Person[NamesSorted.Length];
+for (int i = 0; i < NamesSorted.Length; i++) people[i] = new Person(NamesSorted[i], NamesCalls[i], count); // преобразую полученные два массива в структуру
+
+static void Swap(Person[] array, int i, int j)
+{
+    Person temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
+static void GnomeSort(Person[] inArray) // гномья сортировка для структур
+{
+    int i = 1;
+    int j = 2;
+    while (i < inArray.Length)
+    {
+        if (inArray[i - 1].prop < inArray[i].prop)
+        {
+            i = j;
+            j += 1;
+        }
+        else
+        {
+            Swap(inArray, i - 1, i);
+            i -= 1;
+            if (i == 0)
+            {
+                i = j;
+                j += 1;
+            }
+        }
+    }
+}
+
+GnomeSort(people);
+
+for (int i = people.Length - 1; i > people.Length - 6; i--)
+{
+    people[i].Print();
+}
+struct Person
+{
+    private string _name;
+    private int _calls;
+    private int _count;
+    public double prop {get; private set;}
+    public Person(string name, int calls, int count)
+    {
+        _name = name;
+        _calls = calls;
+        _count = count;
+        prop = (double) _calls * 100 / count;
+    }
+    public void Print()
+    {
+        Console.WriteLine("{0:f0} был выбран {1:f0} раз(а), доля от всех выбранных: {2:f3}%", _name, _calls, prop);
+    }
+}
---
+++ Лекции/Лекции.cs
@@ -0,0 +1,91 @@
+﻿// и в классе, и в структуре, есть поля и методы
+// в классе больше вещей, которые можно хранить
+
+//программа, позволяющая покупателю проверить наличие сумки в магазине и купить её.
+class Bag
+{
+    private string _model;// Encapsulation 
+    private int _size;// Encapsulation
+    public bool InStock { get; private set; }// Abstraction + Encapsulation
+    public int Price { get; private set; }// Abstraction + Encapsulation
+    public Bag(string model, int size) // Abstraction + Encapsulation
+    {
+        _model = model;
+        _size = size;
+        InStock = true;
+        Price = new Random().Next(10, 20) * _size;
+    }
+    public void Print() // Abstraction
+    {
+        if (InStock)
+            Console.WriteLine(_model + " with " + _size + " size costs:" + Price);
+    }
+
+    public bool Purchase(bool payment) // Abstraction + Encapsulation
+    {
+        if (!InStock)
+        {
+            Console.WriteLine("Sold out"); 
+            return false;
+        }
+        else if (!payment)
+        {
+            Console.WriteLine("Not enough money"); 
+            return false;
+        }
+        else
+        {
+            Console.Write($"You bought a bag \"{_model}\"\n"); //\"- add a" into the string \n - jump
+            InStock = false;
+            return true;
+        }
+    }
+}
+
+class Program
+{
+    private struct Customer // Encapsulation (only Program knows about Customer)
+    {
+        public string Name { get; private set; } // Abstraction + Encapsulation 
+        private int _money; // Encapsulation
+        public Customer(string name) // Abstraction
+        {
+            Name = name;
+            _money = new Random().Next(500, 1000);
+        }
+        public bool Pay(int price) // Abstraction + Encapsulation
+        {
+            if (_money > price)
+            {
+                _money -= price;
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+    static void Main()
+    {
+        Customer me = new Customer("Mark");
+        Console.WriteLine(me.Name);
+
+        Bag[] bags =
+        {
+            new Bag("A", 24),
+            new Bag("A4", 28),
+            new Bag("BiX", 21),
+            new Bag("S7", 27)
+        };
+
+        foreach (Bag bag in bags) 
+            bag.Print();
+        foreach (Bag bag in bags)
+            bag.Purchase(me.Pay(bag.Price));
+        /* same:    if (me.Pay(bag.Price)) 
+        *               bag.Purchase(true);      */
+        Console.WriteLine("Rest bags:");
+        foreach (Bag bag in bags) 
+            bag.Print();
+    }
+}
