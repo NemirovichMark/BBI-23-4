733b0c65e8aac20f839d9fa129421748a583cdc5
YakovlevMaksim <m2213144@edu.misis.ru>
2024-02-27 11:21:17 +0300

    1

--- 6Lab1.cs
+++ 6Lab1.cs
@@ -1,52 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class Jumper
+struct Jumper
 {
-    public string Surname { get; set; }
-    public string Team { get; set; }
-    public int Try1 { get; set; }
-    public int Try2 { get; set; }
-    public int Summa { get; set; }
+    private string _surname;
+    private string _team;
+    private int _try1;
+    private int _try2;
+    private int _summa;
 
     public Jumper(string surname, string team, int try1, int try2)
     {
-        Surname = surname;
-        Team = team;
-        Try1 = try1;
-        Try2 = try2;
-        Summa = try1 + try2;
+        _surname = surname;
+        _team = team;
+        _try1 = try1;
+        _try2 = try2;
+        _summa = try1 + try2;
     }
-}
 
-class Program
-{
-    static void Main(string[] args)
+    public string Surname => _surname;
+    public string Team => _team;
+    public int Try1 => _try1;
+    public int Try2 => _try2;
+    public int Summa => _summa;
+
+    public override string ToString()
     {
-        List<Jumper> jumpers = new List<Jumper>();
-        int n = int.Parse(Console.ReadLine());
-        for (int i = 0; i < n; i++)
-        {
-            string[] input = Console.ReadLine().Split(' ');
-            string surname = input[0];
-            string team = input[1];
-            int try1 = int.Parse(input[2]);
-            int try2 = int.Parse(input[3]);
-            jumpers.Add(new Jumper(surname, team, try1, try2));
-        }
-
-        jumpers.Sort((a, b) => b.Summa.CompareTo(a.Summa));
-
-        Console.WriteLine("Протокол соревнований по прыжкам в длину");
-        Console.WriteLine("-----------------------------------------");
-        Console.WriteLine("| Место | Фамилия | Общество | Попытка 1 | Попытка 2 | Сумма |");
-        Console.WriteLine("-----------------------------------------");
-        for (int i = 0; i < n; i++)
-        {
-            Console.WriteLine("| {0} | {1} | {2} | {3} | {4} | {5} |",
-                              i + 1, jumpers[i].Surname, jumpers[i].Team,
-                              jumpers[i].Try1, jumpers[i].Try2, jumpers[i].Summa);
-        }
-        Console.WriteLine("-----------------------------------------");
+        return $"| {_surname} | {_team} | {_try1} | {_try2} | {_summa} |";
     }
 }
+
+
+static void Main(string[] args)
+{
+    List<Jumper> jumpers = new List<Jumper>();
+    int n = int.Parse(Console.ReadLine());
+    for (int i = 0; i < n; i++)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        string surname = input[0];
+        string team = input[1];
+        int try1 = int.Parse(input[2]);
+        int try2 = int.Parse(input[3]);
+        jumpers.Add(new Jumper(surname, team, try1, try2));
+    }
+
+    jumpers.Sort((a, b) => b.Summa.CompareTo(a.Summa));
+
+    Console.WriteLine("Results");
+    Console.WriteLine("-----------------------------------------");
+    Console.WriteLine("| Surname | Team | Try 1 | Try 2 | Total |");
+    Console.WriteLine("-----------------------------------------");
+    foreach (Jumper jumper in jumpers)
+    {
+        Console.WriteLine(jumper);
+    }
+    Console.WriteLine("-----------------------------------------");
+}
+
