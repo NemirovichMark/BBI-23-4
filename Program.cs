5b266a6f6d0827f6732a10f786ae7f16559b815b
chufyreva-miroslava <159892294+chufyreva-miroslava@users.noreply.github.com>
2024-03-19 13:40:27 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,224 +1,77 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
 
-// 1
-
-//    public struct Participant
-//    {
-//        private string surname;
-//        private int group;
-//        private string teachersurname;
-//        private double result;
-
-//        public Participant(string _surname, int _group, string _teachersurname, double _result)
-//        {
-//            surname = _surname;
-//            group = _group;
-//            teachersurname = _teachersurname;
-//            result = _result;
-//        }
-
-//        public string Surname { get { return surname; } }
-//        public int Group { get { return group; } }
-//        public string TeacherSurname { get { return teachersurname; } }
-//        public double Result { get { return result; } }
-
-//        private static int k = 0;
-
-//        public void Print()
-//        {
-//            Console.WriteLine("Surname {0} \t Group {1} \t Teacher's Surname {2} \t Result {3:f1}", Surname, Group, TeacherSurname, Result);
-//            if (Result <= 2) k++;
-//            Console.WriteLine($"{k} women passed the normative");
-//        }
-//    }
-//public class Program
-//{
-//    public static void Main(string[] args)
-//    {
-//        Participant[] person = new Participant[]
-//        {
-//            new Participant("Ivanova", 1, "Ivanov", 2),
-//            new Participant("Petrova", 2, "Petrov", 3),
-//            new Participant("Sidorova", 3, "Sidorov", 1)
-//        };
-
-//        Sort(person);
-
-//        for (int i = 0; i < person.Length; i++)
-//        {
-//            person[i].Print();
-//        }
-//    }
-
-//    public static void Sort(Participant[] arr)
-//    {
-//        for (int i = 1; i < arr.Length; i++)
-//        {
-//            Participant key = arr[i];
-//            int j = i - 1;
-
-//            while (j >= 0 && arr[j].Result > key.Result)
-//            {
-//                arr[j + 1] = arr[j];
-//                j = j - 1;
-//            }
-
-//            arr[j + 1] = key;
-//        }
-//    }
-//}
-
-
-// 2
-
-//public struct Result
-//{
-//    private string surname;
-//    private double[] results;
-
-//    public Result(string surname, double[] results)
-//    {
-//        this.surname = surname;
-//        this.results = results;
-//    }
-
-//    public double BestResult()
-//    {
-//        double bestResult = results[0];
-//        for (int i = 1; i < results.Length; i++)
-//        {
-//            if (results[i] > bestResult)
-//            {
-//                bestResult = results[i];
-//            }
-//        }
-//        return bestResult;
-//    }
-//    public void print()
-//    {
-//        Console.WriteLine($"Surname: {this.surname}");
-//        double bestResult = results[0];
-//        for (int i = 1; i < results.Length; i++)
-//        {
-//            if (results[i] > bestResult)
-//            {
-//                bestResult = results[i];
-//            }
-//        }
-//        Console.WriteLine($"{"Best result:"} {bestResult}");
-//    }
-//}
-
-//public class Program
-//{
-//    public static void Main()
-//    {
-//        Result[] jumpResults = new Result[3];
-//        jumpResults[0] = new Result("Ivanov", new double[] { 5.6, 5.9, 6.1 });
-//        jumpResults[1] = new Result("Petrov", new double[] { 5.8, 6.0, 6.2 });
-//        jumpResults[2] = new Result("Sidorov", new double[] { 5.7, 6.0, 6.3 });
-
-//        Console.WriteLine("Table of resuls:");
-
-//        for (int i = 0; i < jumpResults.Length; i++)
-//        {
-//            jumpResults[i].print();
-//        }
-
-//    }
-//}
-
-
-// 3
-
-struct FootballTeam
+struct Triangle
 {
-    private string name;
-    private int Scored;
-    private int Failed;
-    private int points;
+    public double A;
+    public double B;
+    public double C;
 
-    public FootballTeam(string name)
+    public Triangle(double a, double b, double c)
     {
-        this.name = name;
-        Scored = 0;
-        Failed = 0;
-        points = 0;
+        A = a;
+        B = b;
+        C = c;
     }
 
-    public string Name { get { return name; } set { name = value; } }
-
-    public void Result(int scored, int conceded)
+    public string Type()
     {
-        Scored += scored;
-        Failed += conceded;
-        if (scored > conceded)
-            points += 3;
-        else if (scored == conceded)
-            points += 1;
+        if (A == B && B == C)
+            return "равносторонний";
+        else if (A == B || B == C || A == C)
+            return "равнобедренный";
+        else
+            return "разносторонний";
     }
 
-    public int Points { get { return points; } }
-
-    public int Difference { get { return Scored - Failed; } }
-
-    public static void Print( FootballTeam[] teams)
+    public double Area()
     {
-        Console.WriteLine("Place | Team   | Points");
-        for (int i = 0; i < teams.Length; i++)
-        {
-            Console.WriteLine("{0,5} | {1,-6} | {2}", i + 1, teams[i].Name, teams[i].Points);
-        }
+        double p = (A + B + C) / 2;
+        double area = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        return area;
+    }
+
+    public void Info()
+    {
+        Console.WriteLine($"Стороны: {A}, {B}, {C}");
+        Console.WriteLine($"Тип: {Type()}");
+        Console.WriteLine($"Площадь: {Area()}\n");
     }
 }
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        FootballTeam[] teams = new FootballTeam[]
+        Triangle[] triangles = new Triangle[]
         {
-            new FootballTeam("team1"),
-            new FootballTeam("team2"),
-            new FootballTeam("team3")
+            new Triangle(3, 4, 5),
+            new Triangle(5, 5, 5),
+            new Triangle(4, 4, 3),
         };
 
-        Match(ref teams[0], ref teams[1]);
-        Match(ref teams[0], ref teams[2]);
-        Match(ref teams[1], ref teams[0]);
-        Match(ref teams[1], ref teams[2]);
+        SORT(triangles);
 
-        Sort(teams);
-        FootballTeam.Print(teams);
+        foreach (var triangle in triangles)
+        {
+            triangle.Info();
+        }
     }
-
-    static void Match(ref FootballTeam team1, ref FootballTeam team2)
+    static void SORT(Triangle[] triangles)
     {
-        Random random = new Random();
-        int scored = random.Next(0, 5);
-        int conceded = random.Next(0, 5);
-        team1.Result(scored, conceded);
-        team2.Result(conceded, scored);
-    }
-
-    static void Sort(FootballTeam[] teams)
-    {
-        int n = teams.Length;
+        int n = triangles.Length;
         for (int i = 0; i < n - 1; i++)
         {
             for (int j = 0; j < n - i - 1; j++)
             {
-                if (teams[j].Points < teams[j + 1].Points ||
-                    (teams[j].Points == teams[j + 1].Points && teams[j].Difference < teams[j + 1].Difference))
+                if (triangles[j].Area() < triangles[j + 1].Area())
                 {
-                    FootballTeam temp = teams[j];
-                    teams[j] = teams[j + 1];
-                    teams[j + 1] = temp;
+                    Triangle temp = triangles[j];
+                    triangles[j] = triangles[j + 1];
+                    triangles[j + 1] = temp;
                 }
             }
         }
     }
 }
-
-
 
